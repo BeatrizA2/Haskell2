7e3861e7391d39ae0badeb683e4188a637a6946f
Beatriz de Araújo Andrade <80129016+BeatrizA2@users.noreply.github.com>
2023-07-24 23:40:42 -0300

    .

--- listas.hs
+++ listas.hs
@@ -20,3 +20,24 @@ menorLista [x] = x
 menorLista (x:y:xs)
     | x > y = menorLista (y:xs)
     | otherwise = menorLista (x:xs)
+
+--q4
+fib :: Integer -> Integer
+fib 0  = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+fibTable :: Integer -> String
+fibTable 0 = "n fib n\n0 0"
+fibTable n = fibTable (n - 1)  ++ "\n" ++ show n ++ " " ++ show (fib n)
+
+
+--q5
+measure :: [a] -> Int
+measure [] = -1
+measure [_] = 1
+measure (x:xs) = 1 + measure xs
+
+
+--q6
+takeFinal :: [a] -> Int
